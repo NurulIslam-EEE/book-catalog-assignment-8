4eba4924a7be3e486fbb698e37fa3ca004207e8b
NurulIslam-EEE <nurul.islam02310@gmail.com>
2023-09-13 19:27:02 +0600

    category post,update ,delete route secured for admin

--- src/app/modules/category/category.route.ts
+++ src/app/modules/category/category.route.ts
@@ -1,13 +1,14 @@
 import express, { Router } from "express";
 
 import { CategoryController } from "./category.controller";
+import auth from "../../middleWares/auth";
 
 const router = express.Router();
 
 router.post("/create-category", CategoryController.createCategory);
 router.get("/", CategoryController.getAllCategory);
 router.get("/:id", CategoryController.getSingleCategory);
-router.patch("/:id", CategoryController.updateSingleCategory);
-router.delete("/:id", CategoryController.deleteSingleCategory);
+router.patch("/:id", auth("admin"), CategoryController.updateSingleCategory);
+router.delete("/:id", auth("admin"), CategoryController.deleteSingleCategory);
 
 export const CategoryRoute = router;
