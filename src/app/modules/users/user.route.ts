44f2a17f81a9d118300eeb16650f53961f08fafb
NurulIslam-EEE <nurul.islam02310@gmail.com>
2023-09-13 19:08:14 +0600

    secure get, patch,delete user route for admin

--- src/app/modules/users/user.route.ts
+++ src/app/modules/users/user.route.ts
@@ -1,14 +1,15 @@
 import express, { Router } from "express";
 
 import { UserController } from "./user.controller";
+import auth from "../../middleWares/auth";
 
 const router = express.Router();
 
 router.post("/signup", UserController.createUser);
 router.get("/login", UserController.loginUser);
-router.get("/", UserController.getUsers);
-router.get("/:id", UserController.getSingleUser);
-router.patch("/:id", UserController.updateUser);
-router.delete("/:id", UserController.deleteUser);
+router.get("/", auth("admin"), UserController.getUsers);
+router.get("/:id", auth("admin"), UserController.getSingleUser);
+router.patch("/:id", auth("admin"), UserController.updateUser);
+router.delete("/:id", auth("admin"), UserController.deleteUser);
 
 export const UserRoute = router;
